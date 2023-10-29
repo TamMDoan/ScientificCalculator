2cd31229f859180166bfb42f19b8ea2ede22ec53
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-10-29 00:23:47 -0400

    core functions methods + tests written, scientific ones started

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import java.util.Optional;
 
-import static com.zipcodewilmington.scientificcalculator.Console.addNumbers;
+import static com.zipcodewilmington.scientificcalculator.Console.*;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -30,5 +30,71 @@ public class TestMainApplication {
         Console.displayZero();
     }
 
+    @Test
+    public void testSubtractNumbers(){
+        Double number1 = 5.0;
+        Double number2 = 2.0;
+
+        Double expected = 3.0;
+
+        assertEquals(expected, subtractNumbers(number1, number2));
+    }
+
+    @Test
+    public void testMultiplyNumbers(){
+        Double number1 = 5.0;
+        Double number2 = 2.0;
+
+        Double expected = 10.0;
+
+        assertEquals(expected, multiplyNumbers(number1, number2));
+    }
+
+    @Test
+    public void testDivideNumbers(){
+        Double number1 = 6.0;
+        Double number2 = 2.0;
+
+        Double expected = 3.0;
+
+        assertEquals(expected, divideNumbers(number1, number2));
+    }
+
+    @Test
+    public void testSquareNumber(){
+        Double number1 = 5.0;
+
+        Double expected = 25.0;
+
+        assertEquals(expected, squareNumber(number1));
+    }
+
+    @Test
+    public void testSquareRoot(){
+        Double number1 = 25.0;
+
+        Double expected = 5.0;
+
+        assertEquals(expected, squareRootNumber(number1));
+    }
+
+    @Test
+    public void testPower(){
+        Double number1 = 5.0;
+        Double number2 = 2.0;
+
+        Double expected = 25.0;
+
+        assertEquals(expected, power(number1, number2));
+    }
+
+    @Test
+    public void testInverse(){
+        Double number1 = 5.0;
+
+        Double expected = 0.20;
+
+        assertEquals(expected, inverse(number1));
+    }
 
 }
